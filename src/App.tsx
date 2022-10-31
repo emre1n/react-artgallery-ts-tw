4f83a972eb2ed27c738016bb64c19f014d6d6dfb
Emre Incel <emre1n@protonmail.com>
2022-10-31 17:31:17 +0300

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
