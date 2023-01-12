120cd39938d1f7572615e1164608cf8666b979a2
Emre Incel <emre1n@protonmail.com>
2023-01-12 14:39:31 +0300

    improve navbar css

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,19 +8,19 @@ const Navbar = () => {
   const _routes = Object.values(ROUTES);
 
   return (
-    <nav className="flex justify-between items-center p-4">
+    <nav className="flex justify-between items-center border-b-[1px] h-[100px] px-4">
       <div className="w-24">
         <Link to="/">
           <img className="w-full" src={Logo} alt="Art Gallery" />
         </Link>
       </div>
       <div>
-        <ul className="flex gap-4">
+        <ul className="flex gap-8">
           {_routes
             .filter(route => route.isNavItem)
             .map(route => (
               <li
-                className="font-bold text-slate-500 hover:text-black duration-500 p-2 text-xs md:text-base"
+                className="font-bold text-slate-500 hover:text-black duration-500 text-xs md:text-base"
                 key={route.path}
               >
                 <Link to={route.path}>{route.text}</Link>
