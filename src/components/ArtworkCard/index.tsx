e18e8e3b6a3d3ac7224fa43a60200f5a39f4d3b2
Emre Incel <emre1n@protonmail.com>
2023-01-29 15:42:55 +0300

    refactor discovery component

--- src/components/ArtworkCard/index.tsx
+++ src/components/ArtworkCard/index.tsx
@@ -10,7 +10,7 @@ type TProps = {
 
 const ArtworkCard = ({ artwork }: TProps) => {
   return (
-    <div className="flex flex-col border overflow-hidden rounded-2xl shadow-lg h-[360px]  md:h-[460px]  md:w-[320px] w-auto ">
+    <div className="flex flex-col border overflow-hidden rounded-2xl shadow-lg max-w-[460px] h-[360px]  md:h-[460px]  md:w-[320px]">
       <picture className="overflow-hidden h-3/4 ">
         <img
           src={artwork?.image_url}
--- src/components/Discover/index.tsx
+++ src/components/Discover/index.tsx
@@ -10,27 +10,22 @@ type TProps = {
 
 const Discover = ({ artwork, isLoading }: TProps) => {
   return (
-    // Section Container
-    <div className="flex items-center justify-between h-full">
-      <div className="card p-2 w-11/12 mx-auto">
-        <div className="flex flex-col gap-12 md:flex-row">
-          <div>
-            {isLoading ? (
-              <span>Loading...</span>
-            ) : (
-              <ArtworkCard artwork={artwork} />
-            )}
-          </div>
-          <div className="flex flex-col justify-start md:justify-center max-w-xl md:h-96">
-            <h2 className="items-center text-xl md:text-2xl font-bold p-4">
-              Discover the brilliant works of great artists
-            </h2>
-            <p className="p-4 text-sm">
-              Buy and sell your artworks on our marketplace or try out an
-              English or a Dutch Auction.
-            </p>
-          </div>
-        </div>
+    <div className="flex flex-col gap-12 md:flex-row mx-8">
+      <div className="flex justify-center">
+        {isLoading ? (
+          <span>Loading...</span>
+        ) : (
+          <ArtworkCard artwork={artwork} />
+        )}
+      </div>
+      <div className="flex flex-col justify-start md:justify-center max-w-xl md:h-96">
+        <h2 className="items-center text-xl md:text-2xl font-bold p-4">
+          Discover the brilliant works of great artists
+        </h2>
+        <p className="p-4 text-sm">
+          Buy and sell your artworks on our marketplace or try out an English or
+          a Dutch Auction.
+        </p>
       </div>
     </div>
   );
